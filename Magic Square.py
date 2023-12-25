926807905cd897223ad5aa231d443b87585cd8f3
Arman Kianian <124067637+Armanx200@users.noreply.github.com>
2023-12-25 19:27:59 +0330

    Add files via upload

---
+++ Magic Square.py
@@ -0,0 +1,22 @@
+def MagicSquare(n): #only works for odd numbers
+    magicSquare = [[-1 for _ in range(n)] for _ in range(n)]
+    row, col = n // 2, 0
+
+    for i in range(1, n * n + 1):
+        magicSquare[row][col] = i
+
+        if i % n == 0:     #if magicSquare[row-1][col-1] != 0:
+            col -= 1
+        else:
+            row -= 1
+            col -= 1
+        row = (row + n) % n
+        col = (col + n) % n
+
+    for i in range(n):
+        for j in range(n):
+            print(magicSquare[i][j],end=" ")
+        print()
+
+#---------- Main ----------#
+MagicSquare(5)
